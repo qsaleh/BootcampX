b0a15fb49de083371b6ad76dba0883005f354cf2
qsaleh <qmsaleh@gmail.com>
2020-02-05 22:57:12 +0000

    updated students.js and teachers.js to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,8 +1,5 @@
 const { Pool } = require('pg');
 
-const maxResults = process.argv[3];
-const cohortLike = process.argv[2];
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -10,16 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortLike}%' 
-LIMIT ${maxResults};
-`)
+const maxResults = process.argv[3] || 5;
+const cohortLike = process.argv[2];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const values = [`%${cohortLike}%`, maxResults];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
+  // .catch(err => console.error('query error', err.stack));
 });
-//.catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,21 +8,22 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
 
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${cohortName}: ${teacher.name}`);
   })
-  .catch(err => console.error('query error', err.stack));
 });
 
